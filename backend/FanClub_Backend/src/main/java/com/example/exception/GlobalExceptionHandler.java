14b7b6014ff473b0fb029a6da1d0710347346bad
Chiemi <kair95089@gmail.com>
2026-01-22 09:20:56 +0900

    feat 后端初步框架搭建

---
+++ backend/FanClub_Backend/src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.exception;
+
+import com.example.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public Result handleException(Exception e){
+        log.error("Error:", e);
+        return Result.error("Find error");
+    }
+
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException e){
+        log.error("Error :", e);
+        String message = e.getMessage();
+        int i = message.indexOf("Duplicate entry");
+        String errMsg = message.substring(i);
+        String[] arr = errMsg.split(" ");
+        return Result.error(arr[2] + "Already exist");
+    }
+
+}
+
